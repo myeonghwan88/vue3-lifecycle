b8892cf8cf99200d0a375c57cb155644796ccf74
myeonghwan88 <idmash88@gmail.com>
2022-05-29 21:05:03 +0900

    vue3-lifecycle

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      count: 2
+    }
+  },
+  beforeCreate() {
+    console.log('Befor Create!', this.count) // Befor Create! undefined
+  },
+  created() {
+    console.log('Created!', this.count) // Created! 2
+  },
+  beforeMount() {
+    console.log('Befor Mounte!') // Befor Mounte!
+    console.log(document.querySelector('h1')) // null
+  },
+  mounted() {
+    console.log('Mounted!') // Mounted!
+    console.log(document.querySelector('h1')) // <h1>2</h1>
+  }
+}
+</script>
